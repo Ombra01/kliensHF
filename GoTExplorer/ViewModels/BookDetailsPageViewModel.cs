7ef6841f6e7ab92cd53e1641e03399b4ddea86e1
Kovács Tamás <ombra@freemail.hu>
2020-05-11 20:32:32 +0200

    added character uri to character transformation

--- GoTExplorer/ViewModels/BookDetailsPageViewModel.cs
+++ GoTExplorer/ViewModels/BookDetailsPageViewModel.cs
@@ -20,6 +20,15 @@ namespace GoTExplorer.ViewModels
             set { Set(ref _book, value); }
         }
 
+        public ObservableCollection<Character> Characters { get; set; } = new ObservableCollection<Character>();
+
+        private Character _character;
+        public Character Character
+        {
+            get { return _character; }
+            set { Set(ref _character, value); }
+        }
+
         public override async Task OnNavigatedToAsync(object parameter, NavigationMode mode, IDictionary<string, object> state)
         {
             int intParam = 0;
@@ -28,7 +37,6 @@ namespace GoTExplorer.ViewModels
                 var bookId = (int)parameter;
                 var service = new BookService();
                 Book = await service.GetBookAsync(bookId);
-                await base.OnNavigatedToAsync(parameter, mode, state);
             } else
             {
                 var bookName = (string)parameter;
@@ -39,8 +47,30 @@ namespace GoTExplorer.ViewModels
                 {
                     Book = item;
                 }
-                await base.OnNavigatedToAsync(parameter, mode, state);
+                
             }
+
+            CreateCharactersList();
+
+            await base.OnNavigatedToAsync(parameter, mode, state);
+        }
+
+        private void CreateCharactersList()
+        {
+            foreach (string characterUri in Book.characters)
+            {
+                TransformUriToCharacter(characterUri);
+                Characters.Add(Character);
+            }
+        }
+
+        private async void TransformUriToCharacter(string uri)
+        {
+            string[] urlTokens = uri.Split('/');
+            int characterId = int.Parse(urlTokens[urlTokens.Length - 1]);
+
+            var service = new CharacterService();
+            Character = await service.GetCharacterAsync(characterId);
         }
     }
 }
